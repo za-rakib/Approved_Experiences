e9d58c0b70c07845c5127a990bfd339c8e5a8011
jessanKhan <jessankhan7@gmail.com>
2025-09-14 00:57:38 +0600

    feat: 💄 add tag on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import { Inter, Manrope } from "next/font/google"
 import "./globals.css"
-
+import Script from "next/script";
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
@@ -21,6 +21,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} ${manrope.variable} antialiased`}>
+      <head>
+         <Script
+          id="impact-universal-tag"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(a,b,c,d,e,f,g){e['ire_o']=c;e[c]=e[c]||function(){(e[c].a=e[c].a||[]).push(arguments)};f=d.createElement(b);g=d.getElementsByTagName(b)[0];f.async=1;f.src=a;g.parentNode.insertBefore(f,g);})('https://utt.impactcdn.com/A6477190-e994-4812-b281-2b613f0e188b1.js','script','ire',document,window);`,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
