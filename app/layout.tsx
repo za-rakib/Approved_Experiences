319214b18d034a2117bbc4ad50d0df160e31054b
jessanKhan <jessankhan7@gmail.com>
2025-09-22 22:13:08 +0600

    update: 👍  implemented pearl

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             __html: `(function(a,b,c,d,e,f,g){e['ire_o']=c;e[c]=e[c]||function(){(e[c].a=e[c].a||[]).push(arguments)};f=d.createElement(b);g=d.getElementsByTagName(b)[0];f.async=1;f.src=a;g.parentNode.insertBefore(f,g);})('https://utt.impactcdn.com/A6477190-e994-4812-b281-2b613f0e188b1.js','script','ire',document,window);`,
           }}
         />
+        <script async src="https://tag.pearldiver.io/ldc.js?pid=61672d7dce0af947afcce69b373d23c4&aid=16de03f2"></script>
       </head>
       <body>
         <ClientWrapper>{children}</ClientWrapper>
